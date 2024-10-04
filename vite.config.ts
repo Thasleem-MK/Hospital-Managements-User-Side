aefef3c14d8e6c670c2988cc0b993476aba2bc9e
Mohammed Thasleem MK <mohammedthasleemmk6604@gmail.com>
2024-10-04 09:44:26 +0530

    Testing

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
       registerType: "autoUpdate",
       manifest: {
         name: "Your App Name",
-        short_name: "AppName",
+        short_name: "Medi-Guide",
         description: "Your App Description",
         theme_color: "#ffffff",
         background_color: "#ffffff",
@@ -18,12 +18,12 @@ export default defineConfig({
         start_url: "/",
         icons: [
           {
-            src: "/icons/icon-192x192",
+            src: "/icons/icon-192x192.jpg",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "/icons/icon-512x512",
+            src: "/icons/icon-512x512.jpg",
             sizes: "512x512",
             type: "image/png",
           },
