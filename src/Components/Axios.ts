a76128ecd2b42712857e983fa0f5e9ccb3d73b00
Mohammed Thasleem MK <mohammedthasleemmk6604@gmail.com>
2024-10-22 18:01:52 +0530

    Change Base url

--- src/Components/Axios.ts
+++ src/Components/Axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const apiClient = axios.create({
-  // baseURL: import.meta.env.VITE_AxiosBaseURL,
-  baseURL: "http://localhost:3000",
+  baseURL: import.meta.env.VITE_AxiosBaseURL,
+  // baseURL: "http://localhost:3000",
 });
