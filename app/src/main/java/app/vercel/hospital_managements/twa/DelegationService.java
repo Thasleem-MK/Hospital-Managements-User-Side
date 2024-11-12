4b5fe9effef28ec1a434baf57865bce4a0bb4e74
Mohammed Thasleem MK <mohammedthasleemmk6604@gmail.com>
2024-11-12 16:38:25 +0530

    Updated

---
+++ app/src/main/java/app/vercel/hospital_managements/twa/DelegationService.java
@@ -0,0 +1,22 @@
+package app.vercel.hospital_managements.twa;
+
+
+import com.google.androidbrowserhelper.locationdelegation.LocationDelegationExtraCommandHandler;
+
+import com.google.androidbrowserhelper.playbilling.digitalgoods.DigitalGoodsRequestHandler;
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+            registerExtraCommandHandler(new LocationDelegationExtraCommandHandler());
+        
+            registerExtraCommandHandler(new DigitalGoodsRequestHandler(getApplicationContext()));
+        
+    }
+}
+
