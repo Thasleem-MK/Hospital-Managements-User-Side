480c46c73f39bea4a6f214ada7b75f10a7a3b84b
Mohammed Thasleem MK <mohammedthasleemmk6604@gmail.com>
2024-11-20 22:11:30 +0530

    Edit Handle submit in Contact page

--- src/Components/getCurrentLocation.ts
+++ src/Components/getCurrentLocation.ts
@@ -1,14 +1,3 @@
-// export const getCurrentLocation =  ():[number,number] => {
-//     navigator.geolocation.getCurrentPosition(
-//        (position) => {
-//          return [position.coords.latitude, position.coords.longitude];
-//        },
-//        (error) => {
-//          console.error("Error getting user location:", error);
-//        }
-//      );
-// }
-
 export const getCurrentLocation = (): Promise<[number, number]> => {
   return new Promise((resolve, reject) => {
     navigator.geolocation.getCurrentPosition(
